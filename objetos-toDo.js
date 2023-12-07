ded67b77cf36a1fa6cd4ca55305024cc6b57d665
Dayane <dayanetm@outlook.com>
2023-12-07 16:01:39 -0300

    javascript

---
+++ objetos-toDo.js
@@ -0,0 +1,23 @@
+// Definição de um objeto em Javascript e acesso
+
+const toDos = [
+    {
+        id: 1,
+        description: 'Estudar programação',
+        isCompleted: false
+    },
+
+    {
+        id: 2,
+        description: 'Caminhar com o cachorro',
+        isCompleted: true
+    },
+
+    {
+        id: 3,
+        description: 'Ler um capítulo',
+        isCompleted: false
+    }
+]
+
+console.log(toDos[1].description)
